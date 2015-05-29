192e2a8a36de6af1207e13aa22ae602ed7b62e55
Hardik Vala <hardikvala24@gmail.com>
2015-05-29 13:54:32 +0000

    Allowed for key-based navigation of tagged instances

--- js/controllers/bratController.js
+++ js/controllers/bratController.js
@@ -3,7 +3,17 @@ app.controller('bratController', function($scope, $log, $rootScope, $firebaseObj
   // TODO: Retrieve this value from Firebase.
   $rootScope.numDocuments = 3;
   
-  // Index of document (in document data) to display.
+  // Keycode for selecting previous instance ('a').
+  $scope.prevTaggedKeyCode = 97;
+  // Keycode for selecting next instance ('d').
+  $scope.nextTaggedKeyCode = 100;
+  
+  // Returns the span for the given entity from the text that's loaded.
+  var getSpan = function (entity) {
+    return getEntitySpan($rootScope.docData.text, entity);
+  };
+  
+  /* Index of document (in document data) to display. */
   $rootScope.docIndex = parseInt(getQueryVariable("doc") || "0");
   
   var collDataRef = new Firebase(FB.link + '/collData');
@@ -14,22 +24,6 @@ app.controller('bratController', function($scope, $log, $rootScope, $firebaseObj
   var docDataObjRef = $firebaseObject(docDataRef);
   docDataObjRef.$bindTo($rootScope, 'docData');
   
-  $scope.findVisual = function() {
-    $.find('rect').forEach(function(srcElement) {
-      if (srcElement.attributes && srcElement.attributes.class && srcElement.attributes.class.nodeValue.indexOf("span") > -1) {
-        for (attribute in srcElement.attributes) {
-          if (srcElement.attributes[attribute].nodeName == 'data-span-id') {
-            id = srcElement.attributes[attribute].value
-            if ($rootScope.entity[0] == id) {
-              srcElement.style.strokeWidth = "5";
-              $rootScope.visualElement = srcElement;
-            }
-          }
-        }
-      }
-    });
-  }
-
   collDataObjRef.$loaded(
     function(collData) {
       docDataObjRef.$loaded(function(docData) {
@@ -51,8 +45,13 @@ app.controller('bratController', function($scope, $log, $rootScope, $firebaseObj
             liveDispatcher.on('renderError: Fatal', renderError);
             liveDispatcher.on('doneRendering', $scope.findVisual);
 
-            $rootScope.entity = getFirstEntity();
-            $rootScope.span = getSpan($rootScope.entity);
+            $rootScope.taggedEntities = sortTaggedEntities(docData.entities);
+
+            // DECPRECATED
+            $rootScope.entity = firstTaggedEntity($rootScope.taggedEntities);
+            $rootScope.selectedEntity = firstTaggedEntity($rootScope.taggedEntities);
+            $rootScope.taggedEntityIndex = 0;
+            $rootScope.span = getSpan($rootScope.selectedEntity);
             $rootScope.tags = $rootScope.collData.entity_types;
             
             /* Tags that don't refer specifically to a character. */
@@ -66,7 +65,7 @@ app.controller('bratController', function($scope, $log, $rootScope, $firebaseObj
             });
             
             $rootScope.tagOrdering = $rootScope.tags.map(function (tag) { return tag.type; });
-            $rootScope.visualElement = $scope.findVisual;
+            $scope.selectedVisualElement = $scope.findVisual;
             $rootScope.aliasesRemaining = $rootScope.docData.entities.filter(function(e) {
                 return e[1] == 'ALIAS';
               }).length;
@@ -97,43 +96,57 @@ app.controller('bratController', function($scope, $log, $rootScope, $firebaseObj
     }
   );
 
-  var getFirstEntity = function() {
-    return $rootScope.docData.entities.reduce(function(p, v) {
-      return ((p[2][0][0] < v[2][0][0]) ? p : v);
+  $scope.applySelectionStyle = function (element) {
+    element.style.strokeWidth = '4';
+  };
+  
+  $scope.unapplySelectionStyle = function (element) {
+    element.style.strokeWidth = '';
+  };
+
+  $scope.findVisual = function() {
+    $.find('rect').forEach(function(srcElement) {
+      if (srcElement.attributes && srcElement.attributes.class && srcElement.attributes.class.nodeValue.indexOf("span") > -1) {
+        for (attribute in srcElement.attributes) {
+          if (srcElement.attributes[attribute].nodeName == 'data-span-id') {
+            id = srcElement.attributes[attribute].value
+            if ($rootScope.selectedEntity[0] == id) {
+              $scope.applySelectionStyle(srcElement);
+              $scope.selectedVisualElement = srcElement;
+            }
+          }
+        }
+      }
     });
-  };
-
-  // TODO: Make this accessible in other parts of the code.
-  var getSpan = function(entity) {
-    return $rootScope.docData.text.substring(entity[2][0][0], entity[2][0][1]);
-  };
-
-  var unselect = function() {
-    if ($rootScope.visualElement != '') {
-      $rootScope.visualElement.style.strokeWidth = "";
-    }
-    $rootScope.visualElement = '';
-    $rootScope.entity = '';
-    $rootScope.span ='';
   }
 
-  $rootScope.select = function(e) {
-    unselect();
+  $scope.unselect = function() {
+    if ($scope.selectedVisualElement) {
+      $scope.unapplySelectionStyle($scope.selectedVisualElement);
+    }
+    $scope.selectedVisualElement = null;
+    $rootScope.selectedEntity = null;
+    $rootScope.span = null;
+  }
+
+  $rootScope.selectEntity = function(e) {
+    $scope.unselect();
     if (e.srcElement.nodeName == 'rect' && e.srcElement.attributes.class.nodeValue.indexOf("span") > -1) {
-      for (attribute in e.srcElement.attributes) {
+      for (var attribute in e.srcElement.attributes) {
         if (e.srcElement.attributes[attribute].nodeName == 'data-span-id') {
-          $rootScope.visualElement = e.srcElement;
-          id = e.srcElement.attributes[attribute].value
+          $scope.selectedVisualElement = e.srcElement;
+          var id = e.srcElement.attributes[attribute].value;
           $rootScope.docData['entities'].forEach(function(entity) {
             if (entity[0] == id) {
 
               $rootScope.tags.forEach(function(item) {
                 if (item.type == entity[1]) {
-                  $rootScope.entity = entity;
+                  $rootScope.selectedEntity = entity;
                   $rootScope.span = getSpan(entity);
+                  $rootScope.taggedEntityIndex = findTaggedEntityIndex($rootScope.taggedEntities, $rootScope.selectedEntity);
                 }
               });
-              e.srcElement.style.strokeWidth = "5";
+              $scope.applySelectionStyle(e.srcElement);
             }
           });
         }
@@ -141,6 +154,44 @@ app.controller('bratController', function($scope, $log, $rootScope, $firebaseObj
     }
     else {
       return;
+    }
+  };
+  
+  // Moves to the tagged entity immediately preceding the selected one.
+  var toPrevTaggedEntity = function () {
+    $scope.unselect();
+    
+    $rootScope.taggedEntityIndex--;
+    $rootScope.selectedEntity = $rootScope.taggedEntities[$rootScope.taggedEntityIndex];
+    $rootScope.span = getSpan($rootScope.selectedEntity);
+    
+    $scope.findVisual();
+  }
+  
+  // Moves to the tagged entity immediately succeeding the selected one.
+  var toNextTaggedEntity = function () {
+    $scope.unselect();
+    
+    $rootScope.taggedEntityIndex++;
+    $rootScope.selectedEntity = $rootScope.taggedEntities[$rootScope.taggedEntityIndex];
+    $rootScope.span = getSpan($rootScope.selectedEntity);
+    
+    $scope.findVisual();
+  }
+  
+  // Moves to either the immediately preceding or succeeding tagged entity
+  // relative to the selected one based on the specific key pres.
+  $scope.nextSelection = function ($event) {
+    if ($rootScope.selectedEntity) {
+      if($event.keyCode == $scope.prevTaggedKeyCode) {
+        if ($rootScope.taggedEntityIndex > 0) {
+          toPrevTaggedEntity();
+        }
+      } else if ($event.keyCode == $scope.nextTaggedKeyCode) {
+        if ($rootScope.taggedEntityIndex < $rootScope.taggedEntities.length - 1) {
+          toNextTaggedEntity();
+        }
+      } 
     }
   };
   
--- js/domain.js
+++ js/domain.js
@@ -27,3 +27,77 @@ var isCharacterTag = function (tag) {
         tag.type == TagInfo.other.name ||
         tag.type == TagInfo.unresolved.name);
 };
+
+var ID_IND = 0;
+var TYPE_IND = 1;
+var CHAR_OFFSETS_IND = 2;
+var START_OFFSET_IND = 0;
+var END_OFFSET_IND = 1;
+
+var entityEquals = function (entity1, entity2) {
+    return (entity1[ID_IND] == entity2[ID_IND] &&
+      entity1[TYPE_IND] == entity2[TYPE_IND] &&
+      entity1[CHAR_OFFSETS_IND][0][START_OFFSET_IND] == entity2[CHAR_OFFSETS_IND][0][START_OFFSET_IND] &&
+      entity1[CHAR_OFFSETS_IND][0][END_OFFSET_IND] == entity2[CHAR_OFFSETS_IND][0][END_OFFSET_IND]);
+};
+
+/* Retrieves the span for the given entity from the given text. */
+var getEntitySpan = function(text, entity) {
+    return text.substring(entity[2][0][0], entity[2][0][1]);
+};
+
+/* Sorts the given array of tagged entities by the starting character offset. If
+   the offsets are equal, then the one with the lexicographically greater type
+   is returned. */
+var sortTaggedEntities = function (entities) {
+    return entities.sort(function (e1, e2) {
+         var startOffset1 = e1[CHAR_OFFSETS_IND][0][START_OFFSET_IND];
+         var startOffset2 = e2[CHAR_OFFSETS_IND][0][START_OFFSET_IND];
+         
+         if (startOffset1 - startOffset2 == 0){
+            if (e1[TYPE_IND] < e2[TYPE_IND])
+                return 1;
+            if (e1[TYPE_IND] > e2[TYPE_IND])
+                return -1;
+            
+            return 0;
+         } else
+            return startOffset1 - startOffset2;
+    });
+}
+
+/* Returns the first tagged entity, in terms of character offset, in the given
+   array of entities (Assumed to be unsorted). */
+var firstTaggedEntity = function (entities) {
+    return entities.reduce(function(p, v) { return ((p[2][0][0] < v[2][0][0]) ? p : v); });
+};
+
+/* Returns the last tagged entity, in terms of character offset, in the given
+   array of entities (Assumed to be unsorted). */
+var lastTaggedEntity = function (entities) {
+    return entities.reduce(function(p, v) { return ((p[2][0][0] < v[2][0][0]) ? p : v); });
+};
+
+/* Returns the entity immediately succeeding the given one, in the given list of
+   tagged entities (unsorted). */
+var nextTaggedEntity = function(entities, entity) {
+    var lastEntity = entities.reduce(function(p, v) {
+      return (p[2][0][0] > v[2][0][0]) ? p : v;
+    });
+    
+    return $rootScope.docData.entities.reduce(function(p, v) {
+      return (entity[2][0][0] < v[2][0][0] && v[2][0][0] < p[2][0][0]) ? v : p;
+    }, lastEntity);
+};
+
+/* Returns the index of the givne tagged entity in the array of entities. If it
+   doesn't exist, then -1 is returned. */
+var findTaggedEntityIndex = function (entities, entity) {
+    var i = 0;
+    for (i = 0; i < entities.length; i++) {
+        if (entityEquals(entities[i], entity))
+            return i;
+    }
+    
+    return -1;
+};
